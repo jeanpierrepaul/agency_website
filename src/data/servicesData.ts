aeda0047e37ae2a89c9c10ec39e1918986402753
jeanpierrepaul <122696581+jeanpierrepaul@users.noreply.github.com>
2025-10-01 23:32:14 +0000

    update

--- src/data/servicesData.ts
+++ src/data/servicesData.ts
@@ -1,10 +1,10 @@
 import img1 from '../images/1.jpg';
-import img2 from '../images/2.PNG?url';
+import img2 from '../images/2.png';
 import img3 from '../images/3.png';
 import img4 from '../images/4.png';
 import img5 from '../images/5.png';
 import img6 from '../images/6.png';
-import img7 from '../images/7.PNG?url';
+import img7 from '../images/7.png';
 
 export interface ServiceData {
   id: string;
