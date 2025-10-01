3954d2406fdf4a1bbfccdfa845e4dbf70deb6b08
jeanpierrepaul <122696581+jeanpierrepaul@users.noreply.github.com>
2025-10-01 23:40:11 +0000

    update

--- src/Components/Services/Services.tsx
+++ src/Components/Services/Services.tsx
@@ -8,7 +8,7 @@ import img3 from '../../images/3.png';
 import img4 from '../../images/4.png';
 import img5 from '../../images/5.png';
 import img6 from '../../images/6.png';
-import img7 from '../../images/7.png';
+import img7 from '../../images/7.PNG';
 
 const servicesData = [
     {
